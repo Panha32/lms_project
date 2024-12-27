b6355bca8d41fc48d48ae2633714e1ddc72634b4
Panha32 <panharavuth2526@gmail.com>
2024-12-28 00:36:36 +0700

    add CRUD on book

--- controllers/book.js
+++ controllers/book.js
@@ -1,14 +1,13 @@
 const con = require('../config/db');
 
-
 const getBook = (req, res) => {
     con.query("SELECT * FROM tbl_book", (err, data) => {
-        res.render('tbl_book', {data: data});
+        res.render('book/tbl_book', {data: data});
     })
 }
 
 const getCreateBook = (req, res) => {
-    res.render('frmCreateBook');
+    res.render('book/frmCreateBook');
 }
 
 const postCreateBook = (req, res) => {
@@ -20,6 +19,27 @@ const postCreateBook = (req, res) => {
     })
 }
 
+const getDeleteBook = (req, res) => {
+    con.query("DELETE FROM tbl_book WHERE bookID = ?", req.params.bookID, (err, data) => {
+        res.redirect('/book');
+    })
+}
+
+const getEditBook = (req, res) => {
+    con.query("SELECT * FROM tbl_book WHERE bookID = ?", req.params.bookID, (err, data) => {
+        res.render('book/frmEditBook', {data});
+    })
+}
+
+const postEditBook = (req, res) => {
+    const body = req.body;
+    const sql = "UPDATE tbl_book SET name = ?, authorID = ?, categoryID = ? WHERE bookID = ?";
+    const arrData = [body.name, 0, 0, body.bookID];
+    con.query(sql, arrData, (err, data) => {
+        err ? console.log(err) : res.redirect('/book');
+    })
+}
+
 module.exports = {
-    getCreateBook, getBook, postCreateBook
+    getCreateBook, getBook, postCreateBook, getDeleteBook, getEditBook, postEditBook
 }
--- routes/book.js
+++ routes/book.js
@@ -1,13 +1,21 @@
 const express = require('express');
-const {getCreateBook, getBook, postCreateBook} = require('../controllers/book');
+const {getCreateBook, getBook, postCreateBook, getDeleteBook, getEditBook, postEditBook} = require('../controllers/book');
 
 const route = express.Router();
 
+// get book page
 route.get('/book', getBook);
 
+// create book
 route.get('/createBook', getCreateBook);
-
 route.post('/createBook', postCreateBook);
+
+// delete book
+route.get('/deleteBook/:bookID', getDeleteBook);
+
+// edit book
+route.get('/editBook/:bookID', getEditBook);
+route.post('/editBook', postEditBook);
 
 module.exports = route;
 
