271ad7d7b064da75d8ccece9a7e518d512e26a76
Panha32 <panharavuth2526@gmail.com>
2025-01-06 22:45:45 +0700

    add auth proccess

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,63 @@
 const con = require('../config/db');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-const login = (req, res) => {
+const generateToken = (id) => {
+    return jwt.sign({id}, 'panhajs', {expiresIn: 1 * 24 * 60 * 60});
+}
+
+const loginGet = (req, res) => {
     res.render('auth/login');
 }
 
-const register = (req, res) => {
+const loginPost = (req, res) => {
+    let body = req.body;
+    con.query("SELECT * FROM user WHERE email = ?", body.email, async (err, data) => {
+        if(err) {
+            console.log(err);
+        }
+
+        if(data.length == 0) {
+            res.send("Invalid Email");
+        }
+
+        const comparePassword = await bcrypt.compare(body.password, data[0].password);
+
+        if(comparePassword) {
+            const token = generateToken(data[0].userID);
+            res.cookie('jwt', token, {maxAge: 3 * 24 * 60 * 60 * 1000, httpOnly: true});
+            res.redirect('/index');
+        } else {
+            res.send("Invalid Password");
+        }
+    })
+}
+
+const registerGet = (req, res) => {
     res.render('auth/register');
+}
+
+const registerPost = async (req, res) => {
+    let body = req.body;
+    let salt = await bcrypt.genSalt();
+    let hashPassword = await bcrypt.hash(body.password, salt);
+    
+    const sql = "INSERT INTO `user`(`firstName`, `lastName`, `email`, `password`, `repeatPassword`) VALUES (?, ?, ?, ?, ?)";
+    const arrData = [body.firstName, body.lastName, body.email, hashPassword, hashPassword];
+    con.query(sql, arrData, (err, data) => {
+        err ? console.log(err) : res.redirect('/index');
+    })
 }
 
 const forgotPassword = (req, res) => {
     res.render('auth/forgot-password');
 }
 
+const logout = (req, res) => {
+    res.cookie('jwt', '', {maxAge: 1});
+    res.redirect('/login');
+}
+
 module.exports = {
-    login, register, forgotPassword
+    loginGet, loginPost, registerGet, registerPost, forgotPassword, logout
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const { login, register, forgotPassword } = require('../controllers/auth');
+const { loginGet, loginPost, registerGet, registerPost, forgotPassword, logout } = require('../controllers/auth');
 
 const router = express.Router();
 
-router.get('/login', login);
+router.get('/login', loginGet);
+router.post('/login', loginPost);
 
-router.get('/register', register);
+router.get('/register', registerGet);
+router.post('/register', registerPost);
 
 router.get('/forgot-password', forgotPassword);
+
+router.get('/logout', logout);
 
 module.exports = router;
