2f15213337a9db20ed6b190951c590e694d15882
Panha32 <panharavuth2526@gmail.com>
2025-01-08 21:37:24 +0700

    add comment

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+// Imports
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 
+// Route Imports
 const routeBook = require('./routes/book');
 const routeAuthor = require('./routes/author');
 const routeCategory = require('./routes/category');
 const routeAuth = require('./routes/auth');
 const { requireAuth, checkUser } = require('./middlewares/auth');
 
+// App Configuration
 const app = express();
+
 app.use(fileUpload());
 app.use(cookieParser());
 app.set('view engine', 'ejs');
@@ -16,13 +20,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
+// Global Middleware
 app.get('*', checkUser);
 
-app.get('/', requireAuth, (req, res) => {
-    res.render('index');
-})
-
-app.get('/index', requireAuth, (req, res) => {
+// Main Routes
+app.get(['/', '/index'], requireAuth, (req, res) => {
     res.render('index');
 })
 
@@ -38,10 +40,11 @@ app.get('/blank', (req, res) => {
     res.render('blank');
 })
 
+// Additional Routes
 app.use(routeAuth);
-
 app.use(routeBook);
 app.use(routeAuthor);
 app.use(routeCategory);
 
+// Server Listener
 app.listen(3000);
